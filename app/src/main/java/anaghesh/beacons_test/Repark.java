52e97de80fbc5699b8027de6d60dc4d0325e8ff1
Anaghesh Muruli <anagheshm@gmail.com>
2018-09-25 23:36:42 +0530

    adding Repark activity

---
+++ app/src/main/java/anaghesh/beacons_test/Repark.java
@@ -0,0 +1,13 @@
+package anaghesh.beacons_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Repark extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_repark);
+    }
+}
